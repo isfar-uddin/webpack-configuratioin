a5a38652f8db307292c426e2b669e805d0f7b68f
isfar-uddin <isfarruett@gmail.com>
2018-05-08 16:55:37 +0600

    adding plugins in webconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.export={
 	entry:'src/index.js',
 	output: {
 		path:__dirname + '/dist',
 		filename:'bundle.js'
-	}
+	},
+	plugins:[
+		new HtmlWebpackPlugin()
+	]
+	
 }
